07039a5c10f36d1aabf89e741fc78e90dde830f7
vboiko <volodymyr.boiko@sethq.com>
2015-08-29 15:27:43 +0300

    fdsfsd

--- csv_parser/csv_parser.py
+++ csv_parser/csv_parser.py
@@ -4,7 +4,7 @@
 import csv
 
 
-def make_dict(file_name):
+def make_list(file_name):
     lst = []
     test_lst = _parser(file_name)
     for item in test_lst[1:]:
@@ -24,11 +24,20 @@ def _parser(file_name):
         return []
 
 
+def fff(file_name):
+    """passed_tests_count, failed_tests_count, created_at"""
+    lst = make_list(file_name)
+    for item in lst:
+        return item.items()
+
+
+
 ddd = ['commit_id', 'passed_tests_count', 'pending_tests_count', 'num_workers',
-       'failed_tests_count', 'created_at', 'summary_status',
+       'failed_tests_count', 'created_at', 'spointummary_status',
        'skipped_tests_count', 'session_id', 'started_by', 'started_tests_count',
        'worker_time', 'branch', 'bundle_time', 'duration', 'error_tests_count']
 
 
 if __name__ == '__main__':
-    print make_dict('session_history.csv')[0].keys()
+    print fff('session_history.csv')
+    # print "2014-09-10 05:38:55 UTC"[:10]
