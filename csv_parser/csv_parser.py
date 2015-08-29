375459f18f73bf3dd474e55632c2ea918ed651a8
Volodymyr-Boiko <vbojko86@gmail.com>
2015-08-29 20:53:24 +0300

    create dicts

--- csv_parser/csv_parser.py
+++ csv_parser/csv_parser.py
@@ -2,21 +2,48 @@
 
 
 import csv
+from collections import OrderedDict
 
 
-def make_list(file_name):
-    lst = []
-    test_lst = _parser(file_name)
-    for item in test_lst[1:]:
-        lst.append(dict(zip(test_lst[0], item)))
-    return lst
+def passed_tests(file_name):
+    """passed_tests_count, failed_tests_count, created_at"""
+    lst = _parser(file_name)
+    test_dct = {}
+    date_list = _list_maker(file_name)
+    for item in date_list:
+        s = 0
+        for sec_item in lst:
+            if item == sec_item.get('created_at')[:10]:
+                s += int(sec_item.get('passed_tests_count'))
+                test_dct[sec_item.get('created_at')[:10]] = s
+    return test_dct
+
+
+def failed_tests(file_name):
+    """passed_tests_count, failed_tests_count, created_at"""
+    lst = _parser(file_name)
+    test_dct = {}
+    date_list = _list_maker(file_name)
+    for item in date_list:
+        s = 0
+        for sec_item in lst:
+            if item == sec_item.get('created_at')[:10]:
+                s += int(sec_item.get('failed_tests_count'))
+                test_dct[sec_item.get('created_at')[:10]] = s
+    return test_dct
+
+
+def _list_maker(file_name):
+    lst = _parser(file_name)
+    test_lst = [item.get('created_at')[:10] for item in lst]
+    return list(OrderedDict.fromkeys(test_lst))
 
 
 def _parser(file_name):
     lst = []
     try:
         with open(file_name, 'rb') as csv_file:
-            filereader = csv.reader(csv_file)
+            filereader = csv.DictReader(csv_file)
             for row in filereader:
                 lst.append(row)
         return lst
@@ -24,20 +51,5 @@ def _parser(file_name):
         return []
 
 
-def fff(file_name):
-    """passed_tests_count, failed_tests_count, created_at"""
-    lst = make_list(file_name)
-    for item in lst:
-        return item.items()
-
-
-
-ddd = ['commit_id', 'passed_tests_count', 'pending_tests_count', 'num_workers',
-       'failed_tests_count', 'created_at', 'spointummary_status',
-       'skipped_tests_count', 'session_id', 'started_by', 'started_tests_count',
-       'worker_time', 'branch', 'bundle_time', 'duration', 'error_tests_count']
-
-
 if __name__ == '__main__':
-    print fff('session_history.csv')
-    # print "2014-09-10 05:38:55 UTC"[:10]
+    print failed_tests('session_history.csv')
