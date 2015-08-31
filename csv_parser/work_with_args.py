df7cf0ff8954f3522fae41291bc4ab7e448d8e23
vboiko <volodymyr.boiko@sethq.com>
2015-08-31 16:38:35 +0300

    fsdf

--- csv_parser/work_with_args.py
+++ csv_parser/work_with_args.py
@@ -4,19 +4,32 @@
 import argparse
 
 
+# parser = argparse.ArgumentParser()
+# parser.add_argument("sqare", type=int,
+#                     help="display a square of a given number")
+# parser.add_argument("-v", "--verbose", action="store_true",
+#                     help="increase output verbosity")
+# parser.add_argument('-q', '--qqqq', action="store_true",
+#                     help="increase output verbosity")
+# args = parser.parse_args()
+# answer = args.sqare ** 2
+# # ass = args.square + answer
+# if args.verbose:
+#     print("the square of {} equals {}".format(args.square, answer))
+# # elif args.qqqq:
+# #     print ass
+# else:
+#     print(answer)
+
+
+# def arg_parser():
+#     parser = argparse.ArgumentParser()
+
 parser = argparse.ArgumentParser()
-parser.add_argument("sqare", type=int,
-                    help="display a square of a given number")
-parser.add_argument("-v", "--verbose", action="store_true",
-                    help="increase output verbosity")
-parser.add_argument('-q', '--qqqq', action="store_true",
-                    help="increase output verbosity")
+parser.add_argument("-v")
+parser.add_argument('-b')
 args = parser.parse_args()
-answer = args.sqare ** 2
-# ass = args.square + answer
-if args.verbose:
-    print("the square of {} equals {}".format(args.square, answer))
-# elif args.qqqq:
-#     print ass
-else:
-    print(answer)
+if args.v:
+    print args.v
+elif args.b:
+    print int(args.b) + 4
